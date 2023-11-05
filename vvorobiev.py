c4c7a1bb30c3a6e316b76d9f3f05771d5fea0eda
user1 <vvorobiev@bk.ru>
2023-11-05 11:33:59 +0300

    Vasili Vorobev Added vvorobiev.py and Modified README.md

---
+++ vvorobiev.py
@@ -0,0 +1,17 @@
+# модель ищет ответ на вопрос исходя из заданного текста
+
+!pip install transformers
+
+from transformers import pipeline
+
+question = 'Когда будем убирать лёд?'
+context = ('Вчера выпало много снега, а сегодня из-за'
+         ' тёплой погоды он подтаял и заледенел.'
+         ' Придётся на выходных взять лом и немного разбить лёд.')
+
+model_pipeline = pipeline(
+   task='question-answering',
+   model='timpal0l/mdeberta-v3-base-squad2'
+)
+
+model_pipeline(question=question, context=context)
