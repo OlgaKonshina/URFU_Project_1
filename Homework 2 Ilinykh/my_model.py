41fc62cc50f6ac3144d3b14438b3e0eb24270633
Uika <i-vika00@mail.ru>
2023-11-20 20:53:03 +0500

    my new model

--- Homework 2 Ilinykh/my_model.py
+++ Homework 2 Ilinykh/my_model.py
@@ -1,63 +1,36 @@
-# импортируем библиотеки
-import io
 import streamlit as st
-from PIL import Image
-import torch
-from transformers import AutoTokenizer, ViTFeatureExtractor, VisionEncoderDecoderModel
-tokenizer = AutoTokenizer.from_pretrained("dumperize/movie-picture-captioning")
-feature_extractor = ViTFeatureExtractor.from_pretrained("dumperize/movie-picture-captioning")
-model = VisionEncoderDecoderModel.from_pretrained("dumperize/movie-picture-captioning")
-def load_image():
-    """Создание формы для загрузки изображения"""
-    # Форма для загрузки изображения средствами Streamlit
-    uploaded_file = st.file_uploader(
-        label='Выберите изображение')
-    if uploaded_file is not None:
-        # Получение загруженного изображения
-        image_data = uploaded_file.getvalue()
-        # Показ загруженного изображения на Web-странице средствами Streamlit
-        st.image(image_data)
-        # Возврат изображения в формате PIL
-        return Image.open(io.BytesIO(image_data))
-    else:
-        return None
-exit# обучение модели
-max_length = 128
-num_beams = 4
-gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
+import pandas as pd
+import numpy as np
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+st.title('Uber pickups in NYC')
 
-image_path = 'path/to/image.jpg';
-image = Image.open(image_path)
-image = image.resize([224,224])
-if image.mode != "RGB":
-  image = image.convert(mode="RGB")
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
-pixel_values = feature_extractor(images=[image], return_tensors="pt").pixel_values
-pixel_values = pixel_values.to(device)
+@st.cache_data
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
 
-output_ids = model.generate(pixel_values, **gen_kwargs)
+data_load_state = st.text('Loading data...')
+data = load_data(10000)
+data_load_state.text("Done! (using st.cache_data)")
 
-preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
 
+st.subheader('Number of pickups by hour')
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
 
-# Загружаем предварительно обученную модель
-model = load_model()
-# Выводим заголовок страницы
-st.title('Создание описания к изображению')
-# Выводим форму загрузки изображения и получаем изображение
-img = load_image()
-# Показывам кнопку для запуска распознавания изображения
-result = st.button('Распознать изображение')
-# Если кнопка нажата, то запускаем распознавание изображения
-if result:
-    # Предварительная обработка изображения
-    x = preprocess_image(img)
-    # Распознавание изображения
-    preds = model.predict(x)
-    # Выводим заголовок результатов распознавания жирным шрифтом
-    # используя форматирование Markdown
-    st.write('**Описание к изображению:**')
-    # Выводим результаты распознавания
-    print_predictions(preds)
+# Some number in the range 0-23
+hour_to_filter = st.slider('hour', 0, 23, 17)
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+
+st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+st.map(filtered_data)
