578eb77ad31f5c425ccc1838035cf22ddc2caf8f
Vika00224 <149754943+Vika00224@users.noreply.github.com>
2023-11-21 16:45:41 +0500

    Update my_model.py
    bugfixes

--- Homework 2 Ilinykh/my_model.py
+++ Homework 2 Ilinykh/my_model.py
@@ -1,36 +1,26 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-
-st.title('Uber pickups in NYC')
-
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
-
-@st.cache_data
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
-
-data_load_state = st.text('Loading data...')
-data = load_data(10000)
-data_load_state.text("Done! (using st.cache_data)")
-
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
-
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
-
-# Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
+import torch
+from transformers import T5ForConditionalGeneration, T5Tokenizer
+#загружаем модель
+MODEL_NAME = 'cointegrated/rut5-base-paraphraser'
+model = T5ForConditionalGeneration.from_pretrained(MODEL_NAME)
+tokenizer = T5Tokenizer.from_pretrained(MODEL_NAME)
+model.cuda();
+model.eval();
+#обучение модели 
+@st.cache_resource
+def paraphrase(text, beams=5, grams=4, do_sample=False):
+    x = tokenizer(text, return_tensors='pt', padding=True).to(model.device)
+    max_size = int(x.input_ids.shape[1] * 1.5 + 10)
+    out = model.generate(**x, encoder_no_repeat_ngram_size=grams, num_beams=beams, max_length=max_size, do_sample=do_sample)
+    return tokenizer.decode(out[0], skip_special_tokens=True)
+st.title('Перефраз')# Заголовок в streamlit
+# описание приложения
+st.write('Это приложение можно использовать для перефразирования текста')
+# ввод текста в приложении
+context = st.text_input('Введите текст:')
+# кнопка вывод результата
+result = st.button('Перефразировать')
+# Eсли кнопка нажата запуск модели и вывод результата.
+if result:
+    sor = (paraphrase(context, top_p=1.0, max_length=256))
+    st.write("Новый текст:", *sor)
